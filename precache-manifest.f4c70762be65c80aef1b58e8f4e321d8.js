52680eb4f8218193b58a5941a5c42ae61479f825
William Vitorino <williamvitorino3@gmail.com>
2020-03-08 21:13:18 -0300

    9c7ae0e

---
+++ precache-manifest.f4c70762be65c80aef1b58e8f4e321d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3a45727c3ac748461e2e8690c0a76b",
+    "url": "/web-counter-reactjs/index.html"
+  },
+  {
+    "revision": "2c9ed8dcccae52408a37",
+    "url": "/web-counter-reactjs/static/css/main.d327e4b3.chunk.css"
+  },
+  {
+    "revision": "d571f1f403d42574982a",
+    "url": "/web-counter-reactjs/static/js/2.0289fcf3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/web-counter-reactjs/static/js/2.0289fcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9ed8dcccae52408a37",
+    "url": "/web-counter-reactjs/static/js/main.cbac4f86.chunk.js"
+  },
+  {
+    "revision": "ee150618c2f27db77e31",
+    "url": "/web-counter-reactjs/static/js/runtime-main.966cfc08.js"
+  }
+]);
